ac7e788a82e68d085e755dcc3b177ef82e8e3c79
aetos <aetos382@users.noreply.github.com>
2023-12-24 00:45:07 +0900

    add benchmark

---
+++ VSB.Benchmark/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using VSB;
+
+BenchmarkRunner.Run<ValueStringBuilderBenchmark>();
+
+[MemoryDiagnoser]
+public class ValueStringBuilderBenchmark
+{
+    [Params(10, 100, 1000)]
+    public int InitialLength { get; set; }
+
+    [Params(1, 10, 100)]
+    public int Iteration { get; set; }
+
+    [Benchmark]
+    public int UseStringBuilder()
+    {
+        var initialLength = this.InitialLength;
+        var iteration = this.Iteration;
+
+        var builder = new StringBuilder(initialLength);
+
+        for (int i = 0; i < iteration; ++i)
+        {
+            builder.Append("ABC");
+        }
+
+        return builder.Length;
+    }
+
+    [Benchmark]
+    public int UseValueStringBuilder()
+    {
+        var initialLength = this.InitialLength;
+        var iteration = this.Iteration;
+
+        using var builder = new ValueStringBuilder(stackalloc char[initialLength]);
+        
+        for (int i = 0; i < iteration; ++i)
+        {
+            builder.Append("ABC");
+        }
+
+        return builder.Length;
+    }
+}
